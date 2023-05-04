22ab42a03581dd6df5c9163c39be1faaa3bcb632
Oleksii Sukhanov <bigpapa3225@gmail.com>
2023-05-04 18:17:40 +0300

    add date

--- app.js/code.js
+++ app.js/code.js
@@ -1,1 +1,22 @@
-// const date = ${new Date().getFullYear()}-${new Date().getMonth() < 10? `0${new Date().getMonth() + 1}`: new Date().getMonth() + 1}-${new Date().getDate()}
+
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+let currentMonth = currentDate.getMonth() + 1; // Add 1 to get the correct month (JavaScript months are zero-indexed)
+
+// Check if the month is a single digit
+if (currentMonth < 10) {
+  currentMonth = "0" + currentMonth;
+}
+
+let currentDay = currentDate.getDate();
+if (currentDay < 10) {
+  currentDay = "0" + currentDay;
+}
+
+// Combine the year, month, and day into a string in the format YYYY-MM-DD
+const formattedDate = `${currentDay}-${currentMonth}`;
+
+console.log(formattedDate);
+const dateEl = document.querySelector(".date")
+console.log(dateEl)
+dateEl.innerHTML = formattedDate;
