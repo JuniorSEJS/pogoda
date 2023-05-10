9708203d31e462b80560972ca259297d50919f27
Oleksii Sukhanov <bigpapa3225@gmail.com>
2023-05-10 16:22:45 +0300

    margin

--- app.js/code.js
+++ app.js/code.js
@@ -20,3 +20,16 @@ console.log(formattedDate);
 const dateEl = document.querySelector(".date")
 console.log(dateEl)
 dateEl.innerHTML = formattedDate;
+
+// const meetups = [
+//   { name: "Javascript", isActive: true, members: 100 },
+//   { name: "Angular", isActive: true, members: 900 },
+//   { name: "Node", isActive: false, members: 600 },
+//   { name: "React", isActive: true, members: 500 },
+// ];
+// const membersOnActiveMeetups = (meetups) =>
+//   meetups
+//     .filter((meetup) => meetup.isActive)
+//     .reduce((sum,meetup) => sum + meetup.members,0) 
+
+// console.log(membersOnActiveMeetups(meetups));//1500
